184a8dbc60d89d18e8789cb06d43c4e460ac9c6b
rkiel <cygnus@pobox.com>
2020-04-11 16:16:00 -0400

    experiment: added List

---
+++ src/List.svelte
@@ -0,0 +1,29 @@
+<script>
+  import trim from 'lodash/trim'
+  import concat from 'lodash/concat'
+  import sortBy from 'lodash/sortBy'
+
+  let textValue = ""
+  let items = [ ]
+
+function onClick() {
+  const value = trim(textValue)
+  if (value.length > 0) {
+    items = sortBy(concat(items, {description: value}), ['description'])
+    textValue = ""
+  }
+}
+</script>
+
+<input type="text" bind:value={textValue}/>
+<button on:click={onClick}>Add</button>
+
+<ul>
+  {#each items as item}
+  <li>{item.description}</li>
+  {/each}
+</ul>
+
+<style media="screen">
+
+</style>
